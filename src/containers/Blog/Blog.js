3f159c80e34bde7a2c97b22952cd2508ea9c4304
Bruna Radeke <brunaradeke@hotmail.com>
2020-04-25 13:04:39 +0100

    Making a Post Selectable (9-165)
    Adding postSelectedHandler() by id

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
     //Working with Promises here .then()
     //axios uses promises and GET returns a promise.
@@ -29,10 +30,17 @@ class Blog extends Component {
                 //console.log(response)
             });
     }
+    postSelectedHandler = (idArg) => {
+        this.setState({ selectedPostId: idArg })
+    }
 
     render() {
         const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author} />
+            return <Post
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)} />
         });
         return (
             <div>
@@ -40,7 +48,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost idP={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
