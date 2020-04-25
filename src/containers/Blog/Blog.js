f6296ae4b60c46ba0b550a9a74baeafc5756bfd9
Bruna Radeke <brunaradeke@hotmail.com>
2020-04-25 13:00:28 +0100

    Rendering Fetched Data to the Screen (9-163)

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,23 +7,28 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts: []
+    }
     //Working with Promises here .then()
     //axios uses promises and GET returns a promise.
     //Promises is a method which takes a function as the input and this function will get executed once the promise resolves
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
             .then(response => {
-                console.log(response)
+                this.setState({ posts: response.data });
+                //console.log(response)
             });
     }
 
     render() {
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id} title={post.title} />
+        });
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
