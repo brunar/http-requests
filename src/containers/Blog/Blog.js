07fc7d8a7476dcf2c91e6b6537d98da3e118fec8
Bruna Radeke <brunaradeke@hotmail.com>
2020-04-25 19:33:20 +0100

    Handling Errors Locally - Promise catch(error => {}) (9-170)
    To simulate an error example change the url axios.get

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -6,10 +6,12 @@ import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
+//To simulate an error example change the url axios.get
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
     //Working with Promises here .then()
     //axios uses promises and GET returns a promise.
@@ -28,6 +30,10 @@ class Blog extends Component {
                 })
                 this.setState({ posts: updatePosts });
                 //console.log(response)
+            })
+            .catch(error => {
+                //console.log(error);
+                this.setState({ error: true });
             });
     }
     postSelectedHandler = (idArg) => {
@@ -35,13 +41,18 @@ class Blog extends Component {
     }
 
     render() {
-        const posts = this.state.posts.map(post => {
-            return <Post
-                key={post.id}
-                title={post.title}
-                author={post.author}
-                clicked={() => this.postSelectedHandler(post.id)} />
-        });
+        let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>
+
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)} />
+            });
+        }
+
         return (
             <div>
                 <section className="Posts">
