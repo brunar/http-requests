c62ffa2cfb4b8df40ae0f17c6c958ccd8c22aed1
Bruna Radeke <brunaradeke@hotmail.com>
2020-04-25 12:55:02 +0100

    Creating a Http Request to GET Data (9-162)
    Install Package axios and /Working with Promises .then()

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,7 +7,17 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
+    //Working with Promises here .then()
+    //axios uses promises and GET returns a promise.
+    //Promises is a method which takes a function as the input and this function will get executed once the promise resolves
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                console.log(response)
+            });
+    }
+
+    render() {
         return (
             <div>
                 <section className="Posts">
