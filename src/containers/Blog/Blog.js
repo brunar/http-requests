30b50dd7568bfbc24535c03a65352aabbf4fa51c
Bruna Radeke <brunaradeke@hotmail.com>
2020-04-28 21:14:20 +0100

    Setting Up and Rendering Routes (11-193)

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from '../Posts/Posts';
@@ -16,7 +17,9 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                <Route path="/" exact render={() => <h1>Home</h1>} />
+                {/* Without exact does means any path that starts with / */}
+                <Route path="/" render={() => <h1>Home 2</h1>} />
             </div>
         );
     }
