8e8c91acc855a22b779365edc000004fc55c7308
Bruna Radeke <brunaradeke@hotmail.com>
2020-04-28 21:23:15 +0100

    Switching Between Pages (11-195)

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from '../Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 //To simulate an error example change the url axios.get
 class Blog extends Component {
@@ -21,6 +22,7 @@ class Blog extends Component {
                 {/* Without exact does means any path that starts with /  */}
                 {/* <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
