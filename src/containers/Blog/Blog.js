2bfada12ab91b2363a7d254e75cec46654714b12
Bruna Radeke <brunaradeke@hotmail.com>
2020-04-28 21:18:16 +0100

    Rendering Components for Routes (11-194)

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -17,9 +17,10 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact render={() => <h1>Home</h1>} />
-                {/* Without exact does means any path that starts with / */}
-                <Route path="/" render={() => <h1>Home 2</h1>} />
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
+                {/* Without exact does means any path that starts with /  */}
+                {/* <Route path="/" render={() => <h1>Home 2</h1>} /> */}
+                <Route path="/" exact component={Posts} />
             </div>
         );
     }
