fbe34f4cb968ec5eff72237f2eb073e611e1c528
Bruna Radeke <brunaradeke@hotmail.com>
2020-04-29 17:33:44 +0100

    Styling the Active Route (11-201)
    { Route, NavLink } AND activeClassName="my-active" OR adding activeStyle - For NavLink has to use exact for path to="/"

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from '../Posts/Posts';
@@ -13,13 +13,21 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
-                                //Building a relative path: your domain/something path and plus something
-                                pathname: this.props.match.url + '/new-post',
+                            {/* For NavLink has to use exact for path to="/" */}
+                            <li><NavLink
+                                exact
+                                to="/"
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: 'orange',
+                                    textDecoration: 'underline'
+                                }}
+                            >Home</NavLink></li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
