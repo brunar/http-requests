ea24145861a4c08042911d251081729e969bd39c
Bruna Radeke <brunaradeke@hotmail.com>
2020-04-30 00:50:20 +0100

    Routing and Server Deployment (11-217)

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+      //Example if your site is server at www.mysite.com/my-app and the Route will contnue working
+      //<BrowserRouter basename="my-app">
       <BrowserRouter>
         <div className="App">
           <Blog />
